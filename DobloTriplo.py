a855484caa94306711a0cdaa177eb8bfb243d552
Thaynara Ribeiro <167586227+Thaynara-Ribeiro@users.noreply.github.com>
2025-09-12 22:23:51 -0300

    Adicionando Pastas
    Adicionando Pasta para organizar as listas de exercícios

--- DobloTriplo.py
+++
@@ -1,3 +0,0 @@
-#3 Realiza a leitura de 1 int e imprime o dobro,triplo e metade do valor lido.
-n = int(input('Digite um número: '))
-print('Dobro: {}, Triplo: {}, Metade: {}'.format(n*2,n*3,n/2))
--- ImparPar.py
+++
@@ -1,6 +0,0 @@
-#7 Realiza a leitura de 1 int e apresenta se ele é par ou ímpar.
-n = int(input('Digite um número: '))
-if n % 2 == 0:
-    print('O número {} é PAR'.format(n))
-else:
-    print('o número {} é IMPAR'.format(n))
