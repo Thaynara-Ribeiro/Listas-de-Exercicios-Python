b3c311be560658055d7147d3b34488db36086810
Thaynara Ribeiro <167586227+Thaynara-Ribeiro@users.noreply.github.com>
2026-07-02 16:55:17 -0300

    Adicionada Terceira Lista de Exercícios
    Adicionada Terceira Lista de Exercícios

---
+++ Lista_de_Exercícios03/positivo_ou_negativo.py
@@ -0,0 +1,6 @@
+#Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+numero = int(input("Digite um número qualquer: "))
+if numero < 0:
+    print("O número é NEGATIVO")
+else:
+    print("O número é POSITIVO")
