fc90e78faf28b6d9f75487baf9ca3691e2510882
Thaynara Ribeiro <167586227+Thaynara-Ribeiro@users.noreply.github.com>
2026-07-02 16:35:16 -0300

    Adicionando Terceira Lista de Exercícios
    Adicionando Terceira Lista de Exercícios Python.
    Cada programa possui uma descrição do anunciado internamente.

---
+++ Lista de Exercicios 03/maior_de_dois_numeros.py
@@ -0,0 +1,8 @@
+# Faça um Programa que peça dois números e imprima o maior deles.
+print("---Comparando Números---")
+primeiro_numero = int(input("Digite o 1º número: "))
+segundo_numero = int(input("Digite o 2º número: "))
+if primeiro_numero > segundo_numero:
+    print("O primeiro número e o MAIOR")
+else:
+    print("O segundo número e o MAIOR")
