fac66f50ad178d5f0f6c13cfb343c93eebd26eb6
Thaynara Ribeiro <167586227+Thaynara-Ribeiro@users.noreply.github.com>
2026-07-06 21:06:15 -0300

    Adicionando um novo arquivo
    Adicionando programa que calcula a partir de uma entrada de um usuário se o ano informado é ou não Bissexto. Além disso foi realizado uma implementação de o usuário informar zero para saber referente ao seu ano atual.

--- Lista_de_Exercícios03/turno.py
+++ Lista_de_Exercícios03/turno.py
@@ -1,6 +1,6 @@
 '''Faça um Programa que pergunte em que turno você estuda. Peça para digitar M-matutinoou V-Vespertinoou N-Noturno. Imprima a
 mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso.'''
-turno = input("Qual turno vocês estuda: (M-matutino ou V-Vespertino ou N-Noturno) ").upper()
+turno = input("Qual turno você estuda: (M-matutino ou V-Vespertino ou N-Noturno) ").upper()
 if turno == "M":
     print("Bom Dia!")
 elif turno == "V":
