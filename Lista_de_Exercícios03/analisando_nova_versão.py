10632bc1bd1ef492ab0b61cf00388cfb5c57373e
Thaynara Ribeiro <167586227+Thaynara-Ribeiro@users.noreply.github.com>
2026-07-09 18:02:32 -0300

    Adicionando um novo arquivo
    Adicionando um nova versão do programa analisando.py
    Na versão atual o código apresenta melhorias na leitura dos dados além do alinhamento de condicionais.

--- Lista_de_Exercícios03/analisando_nova_versão.py
+++ Lista_de_Exercícios03/analisando_nova_versão.py
@@ -3,3 +3,46 @@ ser acompanhado de uma frase que diga se o número é:
 par ou ímpar;
 positivo ou negativo;
 inteiro ou decimal.'''
+print('''Menu
+[1] Soma
+[2] Subtração
+[3] Multiplicação
+[4] Divisão''')
+primeiro_numero = float(input("Digite o 1º número: "))
+segundo_numero = float(input("Digite o 2º número: "))
+escolha = int(input("Digite qual sua escolha: "))
+resultado = 0
+operação_valida = True
+if escolha == 1:
+    resultado = primeiro_numero + segundo_numero
+elif escolha == 2:
+    resultado = primeiro_numero - segundo_numero
+elif escolha == 3:
+    resultado = primeiro_numero * segundo_numero
+elif escolha == 4:
+    if segundo_numero != 0:
+        resultado = primeiro_numero / segundo_numero
+    else:
+        print("ERRO - Não é possivel dividir por zero.")
+        operação_valida = False
+else:
+    print("Opção Inválida")
+    operação_valida = False
+
+if operação_valida:
+    print(f"O resultado da operação é: {resultado}")
+
+    if resultado == round(resultado):
+        if resultado % 2 == 0:
+            print("E um número PAR")
+        else:
+            print("E um número IMPAR")
+        print("E um número INTEIRO")
+    else:
+        print("E um número DECIMAL (Não é PAR nem IMPAR)")
+    if resultado > 0 :
+        print("E um número POSITIVO")
+    elif resultado < 0:
+        print("E um número NEGATIVO")
+    else:
+        print("E um número NEUTRO (Zero)")
